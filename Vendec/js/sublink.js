7d19bc9c39dcc638fe7540cbcba01f1bcce888c7
Pavel Vršecký <55760956+siopaulo@users.noreply.github.com>
2023-08-17 01:33:23 +0200

    Delete Vendec directory

--- Vendec/js/sublink.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const projektyLink = document.getElementById("projekty-link");
-    const sublinks = document.querySelector(".sublinks");
-
-    projektyLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      sublinks.classList.toggle("open");
-    });
-  });
